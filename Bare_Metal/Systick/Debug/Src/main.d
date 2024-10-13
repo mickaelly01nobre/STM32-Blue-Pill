7c66690410e0b7dd4eb038c1a8da821aae29d45f
mickaelly01nobre <mickaellyfreitasnobre@gmail.com>
2024-10-13 18:51:10 -0300

    Add files via upload

---
+++ Bare_Metal/Systick/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/core_cm3.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_version.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_compiler.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_gcc.h \
+ /home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/core_cm3.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_version.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_compiler.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Include/cmsis_gcc.h:
+/home/user/STM32CubeIDE/workspace_1.16.0/Systick/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
