35d014ab8e216a4b90f4c4eeec230939c919f0be
mickaelly01nobre <mickaellyfreitasnobre@gmail.com>
2024-11-02 03:01:29 -0300

    Add files via upload

---
+++ Bare_Metal/Others/BM_ADC_2/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/core_cm3.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_version.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_compiler.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_gcc.h \
+ C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/core_cm3.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_version.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_compiler.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Include/cmsis_gcc.h:
+C:/Users/rodol/STM32CubeIDE/workspace_1.14.1/BM_ADC_2/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
